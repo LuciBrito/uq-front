49966c3f505a498742265fdbb8691055bfe3c5c2
LuciBrito <britoluci410@gmail.com>
2025-12-30 01:23:01 -0300

    Nuevos cambios

--- src/LandingBodyC.jsx
+++ src/LandingBodyC.jsx
@@ -1,71 +1,78 @@
-import Button from 'react-bootstrap/Button';
+import { Container } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
-import img from './img/img-body.png';
+
+import asociarte from './img/asociarte.png';
+import servicios from './img/servicios.png';
+import departamentos from './img/departamentos.png';
+import novedades from './img/novedades.png';
+
+
 
 export default function LandingBodyC() {
   return (
-    <div className="h-100 d-flex justify-content-around align-items-center flex-wrap gap-1 landing-container px-4 py-4 py-lg-8">
-        <Card style={{ width: '25rem' }}>
-            <Card.Img variant="top" src={img}/>
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
-            </Card.Body>
-        </Card>
-                <Card style={{ width: '25rem' }}>
-            <Card.Img variant="top" src={img}/>
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
-            </Card.Body>
-        </Card>
-                <Card style={{ width: '25rem' }}>
-            <Card.Img variant="top" src={img}/>
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
-            </Card.Body>
-        </Card>
-        <Card style={{ width: '25rem' }}>
-            <Card.Img variant="top" src={img}/>
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
-            </Card.Body>
-        </Card>
-                <Card style={{ width: '25rem' }}>
-            <Card.Img variant="top" src={img}/>
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
-            </Card.Body>
-        </Card>
-        <Card style={{ width: '25rem' }}>
-            <Card.Img variant="top" src={img}/>
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
-            </Card.Body>
-        </Card>
-
-  </div>
+    <Container className=" landing-container d-flex flex-wrap justify-content-center gap-4 px-8 py-4 py-lg-5  ">
+        <a className="text-decoration-none" href="">
+            <Card className=" card-info " style={{ width:'100%', maxWidth: '25rem' }}>
+                <Card.Img variant="top" src={servicios}/>
+                <Card.Body>
+                    <Card.Title >
+                        <h3 className='info-text-p text-center'>
+                            Servicios
+                        </h3>
+                    </Card.Title>
+                    <Card.Text className='text-center'>
+                        Conoce mas sobre todos los servicios que ofrecemos.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </a>
+        <a className="text-decoration-none" href="">
+            <Card className=" card-info " style={{  width:'100%', maxWidth: '25rem' }}>
+                <Card.Img variant="top" src={departamentos}/>
+                <Card.Body>
+                    <Card.Title>
+                        <h3 className='info-text-p text-center'>
+                            Departamentos
+                        </h3>   
+                    </Card.Title>
+                    <Card.Text className='text-center'>
+                        Descubri los distintos departamentos que conforman nuestra organización.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </a>
+        <a className="text-decoration-none" href="">
+            <Card className=" card-info " style={{  width:'100%', maxWidth: '25rem' }}>
+                <Card.Img variant="top" src={novedades}/>
+                <Card.Body>
+                    <Card.Title>
+                        <h3 className='info-text-p text-center'>
+                        Novedades 
+                        </h3>
+                    </Card.Title>
+                    <Card.Text className='text-center'>
+                        Mantente al día con las últimas novedades y eventos.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </a>
+        <a className="text-decoration-none" href="">
+            <Card className=" card-info " style={{  width:'100%', maxWidth: '25rem' }}>
+                <Card.Img variant="top" src={asociarte}/>
+                <Card.Body>
+                    <Card.Title>
+                        <h3 className='info-text-p text-center'>
+                            Asociate 
+                        </h3>
+                    </Card.Title>
+                    <Card.Text className='text-center'>
+                        Descubre los beneficios de asociarte con nosotros.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </a>
+  </Container>
   );
 }
+/*
+    <div className="h-100 d-flex justify-content-around align-items-center flex-wrap gap-1 landing-container px-6 py-4 py-lg-5"></div> ¨*/
